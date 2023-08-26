6b898b5a1bd6595ac655e628b3de574813c859c5
Akrapovic <akradeveloper@gmail.com>
2023-08-26 13:18:20 +0200

    rollback

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors()); // Habilita CORS para todas las rutas
 
-app.use(express.text());
+app.use(express.json());
 
 // Configura tu transporte de correo SendGrid aquí
 sgMail.setApiKey(
@@ -24,75 +24,14 @@ app.post("/send-email", async (req, res) => {
     html: "<strong>"+ message +"</strong>",
   };
 
-  const emailSentMessageOK = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <style>
-        body {
-          font-family: Arial, sans-serif;
-          text-align: center;
-          background-color: #f4f4f4;
-          width:400px;
-          height: 100px;
-        }
-        .message {
-          background-color: #4CAF50;
-          color: white;
-          padding: 5px;
-          border-radius: 5px;
-          font-size: 12px;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="message">
-        <h1>ENVIADO</h1>
-        <p>Tu mensaje ha sido enviado, serás contestado lo más rápido posible</p>
-      </div>
-    </body>
-    </html>
-  `;
-
-  const emailSentMessageNOOK = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <style>
-        body {
-          font-family: Arial, sans-serif;
-          text-align: center;
-          background-color: #f4f4f4;
-          width:400px;
-          height: 100px;
-        }
-        .message {
-          background-color: red;
-          color: white;
-          padding: 5px;
-          border-radius: 5px;
-          font-size: 12px;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="message">
-        <h1>ERROR</h1>
-        <p>Ha habido un problema, inténtalo de nuevo más tarde</p>
-      </div>
-    </body>
-    </html>
-  `;
-
-
   try {
     await sgMail.send(msg);
-    res.status(200).send(emailSentMessageOK);
+    res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
     console.error("Error sending email:", error);
     res
       .status(500)
-      .send( emailSentMessageNOOK );
+      .json({ message: "An error occurred while sending the email" });
   }
 });
 
